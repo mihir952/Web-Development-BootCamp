999c434208c9de296a836ebde9e98ea78d94edcd
Mihir Kumar <mihir.kumar@valtech.com>
2023-08-22 15:36:35 +0530

    Css and Advanced Css

---
+++ introjs.js
@@ -0,0 +1,74 @@
+//Udemy Question
+
+// function life(age){
+//     var age1=90-age;
+//     var days=age1*365;
+//     var weeks=age1*52;
+//     var months=age1*12;
+//     var res="You have " + days + " days," + " " +weeks +" weeks, and "+ months +" months left.";
+//     console.log(res);
+// }
+// life(56);
+// var arr=[1,2,3,4,5]
+// var newarr=arr.map((item)=>{
+//     return item+1;
+// })
+// console.log(newarr);
+
+
+// var n=Math.random();
+// n=n*6;
+// n=Math.floor(n);
+// console.log(n)
+
+
+// var x=Math.random(1,100);
+// console.log(x)
+
+// const rndInt = Math.floor(Math.random() * 6) 
+//     console.log(rndInt)
+
+// console.log(1==="1")
+
+// var year=2000;
+// if((year%4==0 && year%100==0) || (year%100==0 && year%400==0)){
+//     console.log("Lear Year")
+// }
+// else{
+//     console.log("Non leap-year");
+// }
+
+// var arr=[1,2,3,4,5,6,7,8];
+// arr.push(100);
+// arr.pop()
+// console.log(arr)
+
+// console.log(arr.includes(25));
+
+
+// var name=["Angela", "Ben", "Jenny", "Michael", "Chloe"];
+// var len=name.length;
+// var num=
+// var random=Math.floor(Math.random());
+// console.log(random)
+
+
+//************fibonacci********//
+// var n=10
+// var output=[];
+// if(n==1){
+//     output=[0];
+// }
+// else if(n==2){
+//     output=[0,1];
+// }
+// else{
+//     output=[0,1];
+//     for(var i=2;i<n;i++){
+//         output.push(output[output.length-2] + output[output.length-1]);
+//     }
+// }
+// console.log(output)
+
+
+
